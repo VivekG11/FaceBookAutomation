d3144c35875c2440d1f0a8a8dd1b3a2cf6ab2b7a
Vivek Guntuku <vivek.g@tvsnext.io>
2021-09-15 13:11:50 +0530

    [Automating new user registration]

--- FacebookAutomation/Registration/SignUp.cs
+++ FacebookAutomation/Registration/SignUp.cs
@@ -2,14 +2,19 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
 using System;
 
 namespace FacebookAutomation.Registration
 {
     public class SignUp
     {
+      
+
         public static void NewUserRegistration(IWebDriver driver)
         {
+            
+
             IWebElement CreateButton = driver.FindElement(By.XPath("//*[text()='Create New Account']"));
             CreateButton.Click();
             System.Threading.Thread.Sleep(1000);
@@ -22,27 +27,28 @@ namespace FacebookAutomation.Registration
             lastname.SendKeys("G");
             System.Threading.Thread.Sleep(1000);
 
-            IWebElement reg_email__ = driver.FindElement(By.Name("reg_email__"));
-            reg_email__.SendKeys("vivekvk2903@gmail.com");
+            IWebElement email = driver.FindElement(By.Name("reg_email__"));
+            email.SendKeys("vivekvk2903@gmail.com");
             System.Threading.Thread.Sleep(1000);
 
-            IWebElement ReEnterEmail = driver.FindElement(By.Name("reg_email_confirmation__"));
-            ReEnterEmail.SendKeys("vivekvk2903@gmail.com");
+            IWebElement confirmEmail = driver.FindElement(By.Name("reg_email_confirmation__"));
+            confirmEmail.SendKeys("vivekvk2903@gmail.com");
             System.Threading.Thread.Sleep(1000);
 
-            driver.FindElement(By.Name("reg_passwd__")).SendKeys("Vivek@3434");
+           IWebElement pwd = driver.FindElement(By.Name("reg_passwd__"));
+            pwd.SendKeys("Vivek@3434");
             System.Threading.Thread.Sleep(1000);
 
-            SelectElement element = new SelectElement(driver.FindElement(By.Name("birthday_day")));
-            element.SelectByText("29");
+            SelectElement date = new SelectElement(driver.FindElement(By.Name("birthday_day")));
+            date.SelectByText("29");
             System.Threading.Thread.Sleep(1000);
 
-            SelectElement element1 = new SelectElement(driver.FindElement(By.Id("month")));
-            element1.SelectByValue("3");
+            SelectElement month = new SelectElement(driver.FindElement(By.Id("month")));
+            month.SelectByValue("3");
             System.Threading.Thread.Sleep(1000);
 
-            SelectElement element2 = new SelectElement(driver.FindElement(By.Id("year")));
-            element2.SelectByValue("2000");
+            SelectElement year = new SelectElement(driver.FindElement(By.Id("year")));
+            year.SelectByValue("2000");
             System.Threading.Thread.Sleep(1000);
 
             driver.FindElement(By.ClassName("_58mt")).Click();
